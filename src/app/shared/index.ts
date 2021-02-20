9ebfecc7abbec2c47d4105003f683442d8efd900
philippe SIMO <philippe.guemkamsimo@gmail.com>
2021-02-20 22:21:21 +0100

    Extract back-to-top-button

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -1,5 +1,7 @@
 import {FooterComponent} from './footer/footer.component';
+import {BackToTopComponent} from './back-to-top/back-to-top.component';
 
-export const components: any[] = [FooterComponent];
+export const components: any[] = [FooterComponent,BackToTopComponent];
 
 export * from './footer/footer.component';
+export * from './back-to-top/back-to-top.component';
