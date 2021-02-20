e34db9beeb00271392b3be914c2ab76a7f9d5693
philippe SIMO <philippe.guemkamsimo@gmail.com>
2021-02-20 21:53:02 +0100

    Generate back-to-top button

---
+++ src/app/shared/back-to-top/back-to-top.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'back-to-top-button',
+  templateUrl: './back-to-top.component.html',
+  styleUrls: ['./back-to-top.component.scss']
+})
+export class BackToTopComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import * as fromComponents from './';
+import { BackToTopComponent } from './back-to-top/back-to-top.component';
 
 
 
 @NgModule({
-  declarations: [...fromComponents.components],
+  declarations: [...fromComponents.components, BackToTopComponent],
   imports: [
     CommonModule
   ],
