2208e1db5d3cc72b5b04d0fddd24484ab825931e
philippe SIMO <philippe.guemkamsimo@gmail.com>
2021-02-20 15:10:35 +0100

    add portfolio/contact module

--- src/app/portfolio/portfolio-routing.module.ts
+++ src/app/portfolio/portfolio-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { PortfolioComponent } from './portfolio.component';
 
-const routes: Routes = [{ path: '', component: PortfolioComponent }, { path: 'portfolio/home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }, { path: 'portfolio/solution', loadChildren: () => import('./solution/solution.module').then(m => m.SolutionModule) }, { path: 'portfolio/specimen', loadChildren: () => import('./specimen/specimen.module').then(m => m.SpecimenModule) }];
+const routes: Routes = [{ path: '', component: PortfolioComponent }, { path: 'portfolio/home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }, { path: 'portfolio/solution', loadChildren: () => import('./solution/solution.module').then(m => m.SolutionModule) }, { path: 'portfolio/specimen', loadChildren: () => import('./specimen/specimen.module').then(m => m.SpecimenModule) }, { path: 'portfolio/contact', loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule) }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
