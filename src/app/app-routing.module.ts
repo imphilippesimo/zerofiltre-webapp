b7f27d9b7765376a28d4d661dcd554e0fe717d64
philippe SIMO <philippe.guemkamsimo@gmail.com>
2021-02-20 14:42:25 +0100

    add portfolio module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'portfolio', loadChildren: () => import('./portfolio/portfolio.module').then(m => m.PortfolioModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
