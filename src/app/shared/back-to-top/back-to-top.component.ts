c65f090c00b3c8685fc8d6d26eaed358fb70295c
philippe SIMO <philippe.guemkamsimo@gmail.com>
2021-03-03 00:16:54 +0100

    add font awesome + back to top behavior

--- src/app/shared/back-to-top/back-to-top.component.ts
+++ src/app/shared/back-to-top/back-to-top.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, HostListener } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'back-to-top-button',
@@ -7,7 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BackToTopComponent implements OnInit {
 
-  constructor() { }
+  windowScrolled: boolean;
+
+  constructor(@Inject(DOCUMENT) private document: Document) { }
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    if (window.pageYOffset || this.document.documentElement.scrollTop || this.document.body.scrollTop > 100) {
+      this.windowScrolled = true;
+    } else if (this.windowScrolled && window.pageYOffset || this.document.documentElement.scrollTop || this.document.body.scrollTop < 10) {
+      this.windowScrolled = false;
+    }
+  }
+
+  scrollToTop() {
+    var currentScroll = this.document.documentElement.scrollTop || this.document.body.scrollTop;
+    if (currentScroll > 0) {
+      this.document.body.scrollTop = 0; // For Safari
+      this.document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
+  }
 
   ngOnInit(): void {
   }
